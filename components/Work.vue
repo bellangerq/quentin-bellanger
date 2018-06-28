a13a9f69fce64b8c47ec5bb7a68302173b63898f
Quentin Bellanger <hello@quentin-bellanger.com>
2018-06-28 15:45:17 +0200

    [+] Lazy loading images

--- components/Work.vue
+++ components/Work.vue
@@ -1,7 +1,7 @@
 <template lang="html">
   <div class="project">
     <a :href="work.href" target="_blank" rel="noopener">
-      <img :src="imgPath(work.src)" :alt="work.title + ': ' + work.subtitle">
+      <img @load="loadImg" :data-src="imgPath(work.src)" :alt="work.title + ': ' + work.subtitle">
     </a>
     <h2>{{ work.title }}</h2>
     <h3>{{ work.subtitle }}</h3>
@@ -14,7 +14,19 @@ export default {
   methods: {
     imgPath (imageName) {
       return require(`~/assets/images/${imageName}`)
+    },
+    setImgSrc() {
+      const images = document.querySelectorAll('[data-src]')
+      images.forEach(image => {
+        image.setAttribute('src', image.getAttribute('data-src'))
+      })
+    },
+    loadImg() {
+      event.target.removeAttribute('data-src')
     }
+  },
+  mounted() {
+    this.setImgSrc()
   }
 }
 </script>
@@ -26,12 +38,28 @@ export default {
   margin-bottom: 40px;
 
   a {
+    background: $color-main;
     box-shadow: 0 2px 2px 0 rgba($color-black, 0.10);
     display: block;
     transition: box-shadow 0.3s ease-in-out;
+    width: 100%;
+
+    @media (min-width: 650px) {
+      min-height: 300px;
+    }
 
     &:hover {
       box-shadow: 0 2px 2px 0 rgba($color-black, 0.20);
+    }
+
+    img {
+      border-radius: $radius;
+      opacity: 1;
+      transition: opacity 0.3s ease-in-out;
+
+      &[data-src] {
+        opacity: 0;
+      }
     }
   }
 
