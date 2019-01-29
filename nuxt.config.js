fb9cde80db7113916b4e829fc402942f46290d76
Quentin Bellanger <hello@quentin-bellanger.com>
2019-01-29 09:46:21 +0100

    [+] Update color and og image call

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,21 +3,21 @@ module.exports = {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { name: 'theme-color', content: '#5090ff' },
+      { name: 'theme-color', content: '#c2185b' },
 
       // Facebook
       { name: 'og:site_name', content: 'Quentin Bellanger' },
-      { name: 'og:image', content: 'https://quentin-bellanger.com/og.png' },
+      { name: 'og:image', content: '/og.png' },
 
       // Twitter
       { name: 'twitter:card', content: 'summary_large_image' },
-      { name: 'twitter:image', content: 'https://quentin-bellanger.com/og.png' }
+      { name: 'twitter:image', content: '/og.png' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.png' }
     ]
   },
-  loading: { color: '#5090ff' },
+  loading: { color: '#c2185b' },
 
   css: [
     { src: '@/assets/stylesheets/main.scss', lang: 'scss'},
@@ -35,7 +35,7 @@ module.exports = {
     'start_url': '/?app=true',
     'display': 'standalone',
     'orientation': 'portrait',
-    'theme_color': '#5090ff',
+    'theme_color': '#c2185b',
     'background_color': '#fff',
     'icons': [{
       'src': '@/static/icon.png',
