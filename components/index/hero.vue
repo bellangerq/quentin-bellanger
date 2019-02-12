4ee4bf3808fb7f3c3b5041b9e3cb2a615b893fb6
Quentin Bellanger <hello@quentin-bellanger.com>
2019-02-12 21:57:08 +0100

    [+] Add payfit as reference

--- components/index/hero.vue
+++ components/index/hero.vue
@@ -6,7 +6,7 @@
       </hyper-link>
       <h1>Quentin Bellanger</h1>
     </div>
-    <p>I am a front-end web developer based in Laval, France. The main focus of my work is to build accessible, inclusive and user-friendly web interfaces for people I like to work with. I've collaborate on diverse projects such as <hyper-link href="https://todoist.com/templates/">Todoist</hyper-link> or <hyper-link href="http://cfoconnect.eu">Spendesk</hyper-link>. You can find some of my work on <hyper-link href="https://github.com/bellangerq">GitHub</hyper-link> and drop me a line on <hyper-link href="https://twitter.com/bellanger_q">Twitter</hyper-link>. Otherwise I am a minimalist and I enjoy gaming, eSport, writing and basketball.</p>
+    <p>I am a front-end web developer based in Laval, France. The main focus of my work is to build accessible, inclusive and user-friendly web interfaces for people I like to work with. I've collaborate on diverse projects such as <hyper-link href="https://todoist.com/templates/">Todoist</hyper-link>, <hyper-link href="http://cfoconnect.eu">Spendesk</hyper-link> or <hyper-link href="https://help.payfit.tech/hc/fr/">Payfit</hyper-link>. You can find some of my work on <hyper-link href="https://github.com/bellangerq">GitHub</hyper-link> and drop me a line on <hyper-link href="https://twitter.com/bellanger_q">Twitter</hyper-link>. Otherwise I am a minimalist and I enjoy gaming, eSport, writing and basketball.</p>
   </header>
 </template>
 
