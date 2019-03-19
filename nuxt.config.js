c064b4b8267b96b146774e592f76a48166047337
Quentin Bellanger <hello@quentin-bellanger.com>
2019-03-19 10:26:51 +0100

    [+] Remove eslint

--- nuxt.config.js
+++ nuxt.config.js
@@ -48,17 +48,5 @@ module.exports = {
   plugins: [
     { src: '~plugins/ga', ssr: false },
     { src: '~plugins/hyper-link', ssr: true }
-  ],
-  build: {
-    extend (config, { isDev, isClient }) {
-      if (isDev && isClient) {
-        config.module.rules.push({
-          enforce: 'pre',
-          test: /\.(js|vue)$/,
-          loader: 'eslint-loader',
-          exclude: /(node_modules)/
-        })
-      }
-    }
-  }
+  ]
 }
